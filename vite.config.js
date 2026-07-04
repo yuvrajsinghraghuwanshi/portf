e3641bc86218e492f6685d47719a3ab5add371d2
Yuvraj Singh Raghuwanshi <yuvrajsinghraghuwanshi@Yuvrajs-MacBook-Air.local>
2026-07-04 17:42:09 +0530

    Fix Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    base: `/${env.VITE_REPO_NAME}/`,
-    plugins: [react(), tailwindcss()],
-    server: {
-      open: true,
-      host: false, // Allow access from network devices
-      proxy: {
-        "/api": {
-          target: "http://localhost:5000",
-          changeOrigin: true,
-          secure: true,
-        },
-      },
-    },
-  };
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
 });
